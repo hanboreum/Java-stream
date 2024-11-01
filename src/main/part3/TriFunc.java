22ef7c1e36e6bde7866512f7310d05e232ccde96
fullm <fullmoon0207@naver.com>
2024-11-01 19:44:46 +0900

    refactor: TriFunction 간단히

--- src/main/part3/TriFunc.java
+++ src/main/part3/TriFunc.java
@@ -3,12 +3,11 @@ package main.part3;
 import main.part3.util.TriFunctionItf;
 
 public class TriFunc {
-    public static void main(String[] args) {
-        TriFunctionItf<Integer, Integer, Integer, Integer> add = (Integer a, Integer b, Integer c) -> {
-            return a + b + c;
-        };
 
-        int result = add.apply(1,2,3);
+    public static void main(String[] args) {
+        TriFunctionItf<Integer, Integer, Integer, Integer> add = (a, b, c) -> a + b + c;
+
+        int result = add.apply(1, 2, 3);
         System.out.println(result);
     }
 }
