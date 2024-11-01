d70d741fd077ffdd049deca62874f8be04546ea3
fullm <fullmoon0207@naver.com>
2024-11-01 19:50:52 +0900

    refactor: Supplier 간단히

--- src/main/part4/MySupplier.java
+++ src/main/part4/MySupplier.java
@@ -3,10 +3,9 @@ package main.part4;
 import java.util.function.Supplier;
 
 public class MySupplier {
+
     public static void main(String[] args) {
-        Supplier<String> stringSupplier = () ->{
-            return "hello world";
-        };
+        Supplier<String> stringSupplier = () -> "hello world";
 
         System.out.println(stringSupplier.get());
     }
