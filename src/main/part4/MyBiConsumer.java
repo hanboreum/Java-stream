423873d7dc002b4d5707472a22336035e69f4a73
fullm <fullmoon0207@naver.com>
2024-11-02 01:24:02 +0900

    feat: BiConsumer helper method 사용

--- src/main/part4/MyBiConsumer.java
+++ src/main/part4/MyBiConsumer.java
@@ -1,11 +1,21 @@
 package main.part4;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiConsumer;
 
 public class MyBiConsumer {
+
     public static void main(String[] args) {
-        BiConsumer<Integer, Double> biConsumer = (Integer i, Double d) ->{
-            System.out.println("prosessing" + d + "at index " + i);
-        };
+        BiConsumer<Integer, Double> biConsumer = (i, d) ->
+                System.out.println("prosessing" + d + "at index " + i);
+        List<Double> inputs = Arrays.asList(1.1,3.4, 5.2);
+        processor(inputs, biConsumer);
+    }
+
+    public static <T> void processor(List<T> inputs , BiConsumer<Integer, T> processor ){
+        for(int i=0; i< inputs.size(); i++){
+            processor.accept(i, inputs.get(i));
+        }
     }
 }
