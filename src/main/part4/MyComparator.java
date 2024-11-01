d68b03d4fe842fff9b62b44cf53677ea22d49543
fullm <fullmoon0207@naver.com>
2024-11-02 02:38:24 +0900

    refactor: Comparator 간단히

--- src/main/part4/MyComparator.java
+++ src/main/part4/MyComparator.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import main.part4.model.User;
+
 //Comparator 두 개의 객체를 비교해 정렬하는데 사용
 public class MyComparator {
 
@@ -16,9 +17,10 @@ public class MyComparator {
         System.out.println(users);
 
         //u1의 id 가 더 작을 때는 음수를 return, u1 u2 같다면 0 return,  u2의 id가 더 작을 땐 양수 return
-        Comparator<User> idComparator = (User u1, User u2) ->{
-            return u1.getId() - u2.getId();
-        };
-        Collections.sort(null, null);
+        Comparator<User> idComparator = (u1, u2) -> u1.getId() - u2.getId();
+
+        //id 순으로 정렬
+        Collections.sort(users, idComparator);
+        System.out.println(users);
     }
 }
