33f00bea3d1240e66fdac44cc1d487744b861618
fullm <fullmoon0207@naver.com>
2024-11-01 19:52:21 +0900

    feat: random supplier

--- src/main/part4/MySupplier.java
+++ src/main/part4/MySupplier.java
@@ -6,7 +6,12 @@ public class MySupplier {
 
     public static void main(String[] args) {
         Supplier<String> stringSupplier = () -> "hello world";
-
         System.out.println(stringSupplier.get());
+
+        //랜덤 수 호출
+        Supplier<Double> randomSupplier = () -> Math.random();
+        System.out.println(randomSupplier.get());
+        System.out.println(randomSupplier.get());
+        System.out.println(randomSupplier.get());
     }
 }
