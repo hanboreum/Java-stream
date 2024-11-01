0a6cd34a7827326150f34dc70636076608bc16f1
fullm <fullmoon0207@naver.com>
2024-11-01 21:31:36 +0900

    feat: Consumer helper method 사용

--- src/main/part4/MyConsumer.java
+++ src/main/part4/MyConsumer.java
@@ -1,15 +1,36 @@
 package main.part4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 //Supplier 와는 반대로, input 만 존재하고 return 하지 않음
 //내부에서 프로세스 동작
 public class MyConsumer {
+
     public static void main(String[] args) {
         //String 을 입력 받고 리턴하지 안흥ㅁ
-        Consumer<String> stringConsumer = (String str) ->{
+        Consumer<String> stringConsumer = (String str) -> {
             System.out.println(str);
         };
         stringConsumer.accept("Hello consumer");
+
+        //List 를 consumer 를 통해 하나씩 프로세스.
+        List<Integer> integerInputs = Arrays.asList(4, 3, 5);
+        Consumer<Integer> integerProcessor = (Integer x) ->
+                System.out.println("Processing integer" + x);
+        process(integerInputs, integerProcessor);
+
+        Consumer<Integer> differentProcess = x -> System.out.println("different processing "+ x);
+        process(integerInputs, differentProcess);
+    }
+
+    //inputs 을 process 해 줄 consumer
+    public static void process(List<Integer> inputs, Consumer<Integer> processor) {
+        for (Integer i : inputs) {
+            //integer 를 하나씩 꺼내 processor 을 호출해줌
+            processor.accept(i);
+        }
     }
 }
