edab5b4401cef8d066b10acd0bc40ac3280b6930
fullm <fullmoon0207@naver.com>
2024-11-01 19:37:44 +0900

    refactor: BiFunction 간단하게

--- src/main/part3/BiFunc.java
+++ src/main/part3/BiFunc.java
@@ -3,13 +3,13 @@ package main.part3;
 
 import java.util.function.BiFunction;
 
+//두개의 파라미터
 public class BiFunc {
 
     public static void main(String[] args) {
-
-        BiFunction<Integer, Integer, Integer> add = (Integer x, Integer y) -> {
-            return x + y;
-        };
+        //input 유추 가능, 타입 삭제해도 된다.
+        //바로 return, 중괄호 return 삭제 가능
+        BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
 
         int result = add.apply(5, 10);
         System.out.println(result);
