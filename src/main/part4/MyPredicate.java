10861f2e629732475a5cd99f32f12c33ab8789e3
fullm <fullmoon0207@naver.com>
2024-11-02 01:34:09 +0900

    feat: predicate 기본

---
+++ src/main/part4/MyPredicate.java
@@ -0,0 +1,13 @@
+package main.part4;
+
+import java.util.function.Predicate;
+
+//입력값을 받아 false or true return
+public class MyPredicate {
+    public static void main(String[] args) {
+        Predicate<Integer> isPositive = (Integer x) ->{
+            return x > 0;
+        };
+    }
+
+}
