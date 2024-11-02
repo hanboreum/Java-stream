24eaa66838025212d7267a8c645b2476c2e0c6cc
fullm <fullmoon0207@naver.com>
2024-11-02 16:54:22 +0900

    feat: method reference helper method

--- src/main/part5/methodReference1.java
+++ src/main/part5/methodReference1.java
@@ -1,14 +1,31 @@
 package main.part5;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
- * method reference 기존에 이미 선언되어 있는 메서드를 지정하고 싶을 때.
- * :: 오퍼레이터 사용.
- * 생략이 많기에 사용할 메서드의 매개변수의 타입과 리턴 타입을 미리 숙지
+ * method reference 기존에 이미 선언되어 있는 메서드를 지정하고 싶을 때. :: 오퍼레이터 사용. 생략이 많기에 사용할 메서드의 매개변수의 타입과 리턴 타입을 미리
+ * 숙지
  */
 public class methodReference1 {
+
+    //input: Integer, Integer | output: Integer
+    public static int calculate(int x, int y, BiFunction<Integer, Integer, Integer> operator) {
+        return operator.apply(x, y);
+    }
+
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int subtract (int x, int y){
+        return x - y;
+    }
+
+    public void myMethod(){
+        System.out.println(calculate(10,3, this::subtract));
+    }
     public static void main(String[] args) {
         //ClassName::staticMethodName
         //클래스의 staticMethod 정적 메서드를 지정할 때
@@ -27,6 +44,21 @@ public class methodReference1 {
         boolean helloEqualsHello = equalsToHello.test("hello");
         System.out.println(helloEqualsWorld);
         System.out.println(helloEqualsHello);
+
+        System.out.println(calculate(8, 3, (x, y) -> x + y));
+        System.out.println(calculate(8, 3, (x, y) -> x - y));
+        System.out.println(calculate(8, 3, (x, y) -> x * y));
+
+        //static method: input, input, className, methodName
+        System.out.println(calculate(8,4, methodReference1::multiply));
+
+        //instance method: className 인스턴스 생성
+        methodReference1 instance = new methodReference1();
+        int sub = instance.subtract(5,2);
+        System.out.println(sub);
+        System.out.println(calculate(5,3, instance::subtract));
+
+        instance.myMethod();
     }
 
 }
